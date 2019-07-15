be11fe9a1e01f01282dc96d15b70cb3a367285cd
Ivan Hu <ivan.hu@canonical.com>
2019-07-15 17:16:29 +0800

    sh: create boot entry with efivar instead of uefiop
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- mok_enroll_key.sh
+++ mok_enroll_key.sh
@@ -2,6 +2,8 @@
 
 BIN=MokEnrollKey.efi
 TARBIN=/boot/efi/EFI/ubuntu/MokEnrollKey.efi
+EFIVAR=/sys/firmware/efi/efivars/MokKeyEnroll-e22021f7-3a03-4aea-8b4c-65881a2b8881
+DER=/var/lib/shim-signed/mok/MOK.der
 
 if [ "$(id -u)" -ne 0 ]; then
   echo "Need root privilege."
@@ -15,15 +17,17 @@ fi
 
 cp -f $BIN $TARBIN || exit $?
 
+#set uefi variable with Mok.der
+if [ -f $EFIVAR ]; then
+  echo "find $EFIVAR, remove it"
+  chattr -i $EFIVAR || exit $?
+  rm -f $EFIVAR 
+fi
 
-#set uefi variable with Mok.der with uefiop application 
-git clone https://github.com/Ivanhu5866/uefiop > /dev/null 2>&1
-cd uefiop
-make
-cd ./bin
-./uefivarset -g e22021f7-3a03-4aea-8b4c-65881a2b8881 -n MokKeyEnroll -f /var/lib/shim-signed/mok/MOK.der > /dev/null 2>&1
-cd ../../
-rm -rf uefiop
+printf "\x07\x00\x00\x00" > temp.der
+cat $DER >> temp.der
+cp -f temp.der $EFIVAR || exit $?
+rm -f temp.der
 
 # Check and delete if mok_enroll_key already
 BOOTNUM=`efibootmgr -v | grep 'mok_enroll_key' | cut -d ' ' -f1 | tr -d [BootOOT*]`
