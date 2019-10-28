e7f1e5bd18ac2683e6773c78aef0cc5a8d1dfad0
Ivan Hu <ivan.hu@canonical.com>
2019-10-28 18:51:26 +0800

    script: add script for enabling SecureBoot
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- mok_enroll_key.sh
+++ mok_enroll_key.sh
@@ -6,6 +6,7 @@ TARBIN=/boot/efi/EFI/ubuntu/MokEnrollKey.efi
 TARLOADBIN=/boot/efi/EFI/ubuntu/UbuntuSecBoot.efi
 EFIVAR=/sys/firmware/efi/efivars/MokKeyEnroll-e22021f7-3a03-4aea-8b4c-65881a2b8881
 DER=/var/lib/shim-signed/mok/MOK.der
+EFIVAR_SB=/sys/firmware/efi/efivars/MokSBEnable-21e2d0b5-ea3a-4222-85e6-8106ad766df0
 
 if [ "$(id -u)" -ne 0 ]; then
   echo "Need root privilege."
@@ -36,6 +37,17 @@ fi
 printf "\x07\x00\x00\x00" > temp.der
 cat $DER >> temp.der
 cp -f temp.der $EFIVAR || exit $?
+rm -f temp.der
+
+#set uefi variable with sb enable
+if [ -f $EFIVAR_SB ]; then
+  echo "find $EFIVAR_SB, remove it"
+  chattr -i $EFIVAR_SB || exit $?
+  rm -f $EFIVAR_SB 
+fi
+
+printf "\x07\x00\x00\x00\x01" > temp.der
+cp -f temp.der $EFIVAR_SB || exit $?
 rm -f temp.der
 
 # Check and delete if mok_enroll_key already
